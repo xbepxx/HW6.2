c092ee16df69e495baf23422f7c218864b12dd39
xbepxx@gmail.com <xbepxx@gmail.com>
2021-10-22 12:39:49 +0300

    Добавьте файлы проекта.

---
+++ HW6.2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW6._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите предложение. Предложение представляет собой слова, разделенные пробелом. Знаки препинания не используются.");
+            string textinsert = Console.ReadLine();
+            textinsert = textinsert.Replace(" ", "");
+            char[] charArray = textinsert.ToCharArray();
+            bool a = true;
+            for (int i = 0; i < charArray.Length/2; i++)
+            {
+                if (charArray[i] != charArray[charArray.Length - i - 1])
+                {
+                    a = false;
+                }
+            }
+            Console.WriteLine(((a) ? "Предложение является" : "Предложение не является") + " палиндромом");
+            Console.ReadKey();
+        }
+    }
+}
